80ef6a9f0346e171d7ba8d652a7f376f97c19c61
shawn160803 <113343054+shawn160803@users.noreply.github.com>
2022-11-12 12:11:59 +0530

    Update new.js

--- new.js
+++ new.js
@@ -49,28 +49,28 @@ details.innerHTML=table
 const saveUserForm = (event)=>{
 event.preventDefault();
 const FullName = document.getElementById('name').value
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-const dob = document.getElementById('dob').value
-const acceptTerms = document.getElementById('acceptTerms').checked
+const email = document.getElementById('Email').value
+const password = document.getElementById('Password').value
+const dob = document.getElementById('DOB').value
+const acceptTerms = document.getElementById('ACCEPTEDTERMS').checked
 var currentYear = new Date().getFullYear();
 var birthYear = dob.split("-");
 let year=birthYear[0]
 var age = currentYear-year
 console.log({age,currentYear,birthYear})
-if(age < 18 || age > 55){
+if(age < 18 || age > 56){
     document.getElementById('dob').style='border:1px solid red'
-  return  alert("Age must be between 18 and 55")
+  return  alert("age must be between 18 and 56")
 
 }else{
     document.getElementById('dob').style='border:none'
 
     const entry ={
         FullName,
-        email,
-        password,
-        dob,
-        acceptTerms
+        Email,
+        Password,
+        DOB,
+        ACCEPTED TERMS
      }
      userEntries.push(entry);
      localStorage.setItem("userEntries",JSON.stringify(userEntries))
